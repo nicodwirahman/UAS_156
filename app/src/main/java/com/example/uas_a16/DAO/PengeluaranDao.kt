8e41c0068dc619878289b325f539164b0098e643
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:16:05 +0700

    membuat insertt updata delete dao pengeluaran

--- app/src/main/java/com/example/uas_a16/DAO/PengeluaranDao.kt
+++ app/src/main/java/com/example/uas_a16/DAO/PengeluaranDao.kt
@@ -1,4 +1,28 @@
 package com.example.uas_a16.DAO
 
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.uas_a16.model.Pengeluaran
+import retrofit2.http.Headers
+
+
+@Dao
 interface PengeluaranDao {
+
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    // Menambahkan data pengeluaran
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPengeluaran(pengeluaran: Pengeluaran)
+
+    // Menghapus data pengeluaran
+    @Delete
+    suspend fun deletePengeluaran(pengeluaran: Pengeluaran)
+        // Mengupdate data pengeluaran
+    @Update
+    suspend fun updatePengeluaran(pengeluaran: Pengeluaran)
 }
