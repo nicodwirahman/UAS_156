1ba372df9bee7ddb8422f17c350d08f0810bdbd0
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:57:55 +0700

    membuat updateScreen

--- app/src/main/java/com/example/uas_a16/ui/view/pengeluaran/UpdatePengeluaranView.kt
+++ app/src/main/java/com/example/uas_a16/ui/view/pengeluaran/UpdatePengeluaranView.kt
@@ -4,17 +4,70 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uas_a16.ui.ViewModel.pengeluaran.InsertPengeluaranEvent
 import com.example.uas_a16.ui.ViewModel.pengeluaran.InsertPengeluaranUiState
+import com.example.uas_a16.ui.ViewModel.pengeluaran.UpdatePengeluaranViewModel
+import com.example.uas_a16.ui.navigasi.CostumeTopAppBar
+import com.example.uas_a16.ui.navigasi.DestinasiUpdate
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate: () -> Unit,
+    viewModel: UpdatePengeluaranViewModel = viewModel()
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = "Update Pengeluaran", // Mengganti penggunaan titleRes
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.updateUiState,
+            onPengeluaranValueChange = viewModel::updateInsertPengeluaranState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePengeluaran()
+                    onNavigate()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
