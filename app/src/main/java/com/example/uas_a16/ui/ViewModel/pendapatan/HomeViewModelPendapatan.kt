e642c7f1ea98b7a8b1f37f565a59bd38e0862e6b
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:48:35 +0700

    membuat HomePendapatanUiState

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/pendapatan/HomeViewModelPendapatan.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/pendapatan/HomeViewModelPendapatan.kt
@@ -1,2 +1,19 @@
 package com.example.uas_a16.ui.ViewModel.pendapatan
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.uas_a16.Repository.PendapatanRepository
+import com.example.uas_a16.model.Pendapatan
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomePendapatanUiState {
+    data class Success(val pendapatan: List<Pendapatan>) : HomePendapatanUiState()
+    object Error : HomePendapatanUiState()
+    object Loading : HomePendapatanUiState()
+}
+
