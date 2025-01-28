807f61156d1d0999c3806b8b8b71da9587f84dcd
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 17:00:04 +0700

    tambahkan sealed class UpdatePendapatanUiState untuk mengelola state UI

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/pendapatan/UpdataeViewModelPendapatan.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/pendapatan/UpdataeViewModelPendapatan.kt
@@ -1,2 +1,8 @@
 package com.example.uas_a16.ui.ViewModel.pendapatan
 
+// Sealed class untuk state UI
+sealed class UpdatePendapatanUiState {
+    object Success : UpdatePendapatanUiState()
+    object Error : UpdatePendapatanUiState()
+    object Loading : UpdatePendapatanUiState()
+}
