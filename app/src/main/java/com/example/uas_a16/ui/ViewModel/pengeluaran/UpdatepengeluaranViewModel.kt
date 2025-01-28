ea99011c9a459730d7ddf2969b844bf4f027c134
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 17:52:48 +0700

    fungsi updatepengeluaranView

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/pengeluaran/UpdatepengeluaranViewModel.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/pengeluaran/UpdatepengeluaranViewModel.kt
@@ -5,10 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-
+import androidx.lifecycle.viewModelScope
 import com.example.uas_a16.Repository.PengeluaranRepository
 import com.example.uas_a16.ui.navigasi.DestinasiUpdate
-
+import kotlinx.coroutines.launch
 
 class UpdatePengeluaranViewModel(
     savedStateHandle: SavedStateHandle,
@@ -17,8 +17,7 @@ class UpdatePengeluaranViewModel(
     var updateUiState by mutableStateOf(InsertPengeluaranUiState())
         private set
 
-    private val _Aset: Int =
-        checkNotNull(savedStateHandle[DestinasiUpdate.route]) // Memastikan ID pengeluaran ada di SavedState
+    private val _Aset: Int = checkNotNull(savedStateHandle[DestinasiUpdate.route]) // Memastikan ID pengeluaran ada di SavedState
 
     init {
 
@@ -29,7 +28,23 @@ class UpdatePengeluaranViewModel(
             }
         }
     }
+
+
+
+
+    fun updateInsertPengeluaranState(insertPengeluaranEvent: InsertPengeluaranEvent) {
+        updateUiState = InsertPengeluaranUiState(insertPengeluaranEvent = insertPengeluaranEvent)
+    }
+
+    suspend fun updatePengeluaran() {
+        viewModelScope.launch {
+            try {
+
+                pengeluaranRepository.updatePengeluaran(updateUiState.insertPengeluaranEvent.toPengeluaran())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
-
-
 
