6ae2c26e6da6bc23c9778391fd613a1fbe58f3d5
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:37:47 +0700

    membuat table Pendapatan

--- app/src/main/java/com/example/uas_a16/model/Pendapatan.kt
+++ app/src/main/java/com/example/uas_a16/model/Pendapatan.kt
@@ -1,2 +1,15 @@
 package com.example.uas_a16.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "pendapatan")
+data class Pendapatan(
+    @PrimaryKey(autoGenerate = true) val idPendapatan: Int = 0,
+    val idAset: Int,
+    val idKategori: Int,
+    val tanggalTransaksi: String, // Format: YYYY-MM-DD
+    val total: Double,
+    val catatan: String
+)
