146734acc9cba554c222978db7e4310d4939d11e
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 17:00:58 +0700

    ViewModel untuk mengelola update pendapatan

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/pendapatan/UpdataeViewModelPendapatan.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/pendapatan/UpdataeViewModelPendapatan.kt
@@ -1,8 +1,21 @@
 package com.example.uas_a16.ui.ViewModel.pendapatan
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.uas_a16.Repository.PendapatanRepository
+
 // Sealed class untuk state UI
 sealed class UpdatePendapatanUiState {
     object Success : UpdatePendapatanUiState()
     object Error : UpdatePendapatanUiState()
     object Loading : UpdatePendapatanUiState()
+}
+// ViewModel untuk mengelola update pendapatan
+class UpdatePendapatanViewModel(private val pendapatanRepository: PendapatanRepository) : ViewModel() {
+
+    var updatePendapatanUiState = mutableStateOf<UpdatePendapatanUiState>(UpdatePendapatanUiState.Loading)
+        private set
+
+    var insertUiState = InsertPendapatanUiState()
+        private set
 }
