dc224448a7c9806099682f8b532c7a8b095eaa46
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:38:16 +0700

    membuat table kategori

--- app/src/main/java/com/example/uas_a16/model/kategori.kt
+++ app/src/main/java/com/example/uas_a16/model/kategori.kt
@@ -1,2 +1,11 @@
 package com.example.uas_a16.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "kategori")
+data class Kategori(
+    @PrimaryKey(autoGenerate = true) val idKategori: Int = 0,
+    val namaKategori: String
+)
+
