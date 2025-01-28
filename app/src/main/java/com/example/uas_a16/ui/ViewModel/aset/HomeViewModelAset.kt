8cbce089ac2857cd020ae23d8db0c17b04b26fdc
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:49:34 +0700

    membuat sealed class homeadetuistate

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/aset/HomeViewModelAset.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/aset/HomeViewModelAset.kt
@@ -1,2 +1,10 @@
-package com.example.uas_a16.ui.ViewModel.aset
+
+import com.example.uas_a16.model.Aset
+
+
+sealed class HomeAsetUiState {
+    data class Success(val aset: List<Aset>) : HomeAsetUiState()
+    object Error : HomeAsetUiState()
+    object Loading : HomeAsetUiState()
+}
 
