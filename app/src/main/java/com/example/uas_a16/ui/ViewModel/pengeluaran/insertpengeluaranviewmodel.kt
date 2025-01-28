b8dd42b8d876658a95f07e12ba896f09d25eaea9
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:04:46 +0700

    Refaktor data class dan fungsi pemetaan.

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/pengeluaran/insertpengeluaranviewmodel.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/pengeluaran/insertpengeluaranviewmodel.kt
@@ -1,2 +1,43 @@
 package com.example.uas_a16.ui.ViewModel.pengeluaran
 
+import com.example.uas_a16.model.Pengeluaran
+
+
+
+
+data class InsertPengeluaranUiState(
+    val insertPengeluaranEvent: InsertPengeluaranEvent = InsertPengeluaranEvent(),
+    val isSuccess: Boolean = false,
+    val successMessage: String = "",
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+    val showDialog: Boolean = false // Untuk menampilkan dialog konfirmasi
+)
+
+data class InsertPengeluaranEvent(
+    val idAset: Int = 0,
+    val idKategori: Int = 0,
+    val total: Double = 0.0,
+    val tanggalTransaksi: String = "", // default to current date
+    val catatan: String = ""
+)
+
+fun InsertPengeluaranEvent.toPengeluaran(): Pengeluaran = Pengeluaran(
+    idAset = idAset,
+    idKategori = idKategori,
+    total = total,
+    tanggalTransaksi = tanggalTransaksi,
+    catatan = catatan
+)
+
+fun Pengeluaran.toUiStatePengeluaran(): InsertPengeluaranUiState = InsertPengeluaranUiState(
+    insertPengeluaranEvent = toInsertPengeluaranEvent()
+)
+
+fun Pengeluaran.toInsertPengeluaranEvent(): InsertPengeluaranEvent = InsertPengeluaranEvent(
+    idAset = idAset,
+    idKategori = idKategori,
+    total = total,
+    tanggalTransaksi = tanggalTransaksi,
+    catatan = catatan
+)
