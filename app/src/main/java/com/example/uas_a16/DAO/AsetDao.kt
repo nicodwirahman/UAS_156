dc3437adc935892d128f430894f75a25caf61b91
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:06:02 +0700

    membuat asetDao

--- app/src/main/java/com/example/uas_a16/DAO/AsetDao.kt
+++ app/src/main/java/com/example/uas_a16/DAO/AsetDao.kt
@@ -1,4 +1,19 @@
 package com.example.uas_a16.DAO
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.uas_a16.model.Aset
+import retrofit2.http.Headers
+
+@Dao
 interface AsetDao {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    // Mendapatkan seluruh data Aset
+    @Query("SELECT * FROM aset")
+    fun getAllAset(): LiveData<List<Aset>>
+
 }
