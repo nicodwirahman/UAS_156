ee058c3b65680e55fccfcd5dfd129c4e1bf23d20
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:13:49 +0700

    membuat pendapatanDao dan insert

--- app/src/main/java/com/example/uas_a16/DAO/PendapatanDao.kt
+++ app/src/main/java/com/example/uas_a16/DAO/PendapatanDao.kt
@@ -1,4 +1,22 @@
 package com.example.uas_a16.DAO
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.uas_a16.model.Pendapatan
+import com.example.uas_a16.model.Pengeluaran
+import retrofit2.http.Headers
+
+@Dao
 interface PendapatanDao {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    // Menambahkan data pendapatan
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPendapatan(pendapatan: Pendapatan)
+
+
 }
