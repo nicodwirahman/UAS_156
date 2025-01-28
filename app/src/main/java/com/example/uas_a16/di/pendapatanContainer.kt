7704e05b0f4343da46fb35cd21f7a67ef9c7bc01
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:55:45 +0700

    PendapatanContainer

--- app/src/main/java/com/example/uas_a16/di/pendapatanContainer.kt
+++ app/src/main/java/com/example/uas_a16/di/pendapatanContainer.kt
@@ -1,30 +1,61 @@
-package com.example.uas_a16.dependenciesinjection
-
-import com.example.uas_a16.Repository.KeuanganRepository
-import com.example.uas_a16.Repository.NetworkKeuanganRepository
-import com.example.uas_a16.service_api.KeuanganService
+import com.example.uas_a16.DAO.AsetDao
+import com.example.uas_a16.DAO.KategoriDao
+import com.example.uas_a16.DAO.PendapatanDao
+import com.example.uas_a16.DAO.PengeluaranDao
+import com.example.uas_a16.Repository.AsetRepository
+import com.example.uas_a16.Repository.KategoriRepository
+import com.example.uas_a16.Repository.PendapatanRepository
+import com.example.uas_a16.Repository.PengeluaranRepository
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import retrofit2.create
 
 interface AppContainer {
-    val keuanganRepository: KeuanganRepository
+    val pendapatanRepository: PendapatanRepository
+    val asetRepository: AsetRepository
+    val pengeluaranRepository: PengeluaranRepository
+    val kategoriRepository: KategoriRepository
 }
 
-class KeuanganContainer : AppContainer {
-    private val baseUrl = "http://10.2.2:3000/api/keuangan/"
+class PendapatanContainer : AppContainer {
+    private val baseUrl = "http://10.2.2:3000/api/" // Sesuaikan baseUrl sesuai kebutuhan
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
         .build()
 
-    private val keuanganService: KeuanganService by lazy {
-        retrofit.create(KeuanganService::class.java)
+    // Aset
+    private val asetDao: AsetDao by lazy {
+        retrofit.create(AsetDao::class.java)
+    }
+    override val asetRepository: AsetRepository by lazy {
+        AsetRepository(asetDao)
     }
 
-    override val keuanganRepository: KeuanganRepository by lazy {
-        NetworkKeuanganRepository(keuanganService)
+    // Pendapatan
+    private val pendapatanDao: PendapatanDao by lazy {
+        retrofit.create(PendapatanDao::class.java)
+    }
+    override val pendapatanRepository: PendapatanRepository by lazy {
+        PendapatanRepository(pendapatanDao)
+    }
+
+    // Pengeluaran
+    private val pengeluaranDao: PengeluaranDao by lazy {
+        retrofit.create(PengeluaranDao::class.java)
+    }
+    override val pengeluaranRepository: PengeluaranRepository by lazy {
+        PengeluaranRepository(pengeluaranDao)
+    }
+
+    // Kategori
+    private val kategoriDao: KategoriDao by lazy {
+        retrofit.create(KategoriDao::class.java)
+    }
+    override val kategoriRepository: KategoriRepository by lazy {
+        KategoriRepository(kategoriDao)
     }
 }
