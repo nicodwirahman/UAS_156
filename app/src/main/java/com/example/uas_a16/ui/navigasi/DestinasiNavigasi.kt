a7f4e1e779aa1c6b2526c09c3c8a4fdc2a51ce71
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:55:13 +0700

    DestinasiNavigasi

--- app/src/main/java/com/example/uas_a16/ui/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/uas_a16/ui/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,47 @@
 package com.example.uas_a16.ui.navigasi
 
-interface DestinasiNavigasi {
+interface AlamatNavigasi {
+    val route: String
+}
+
+
+
+object DestinasiPendapatan : AlamatNavigasi {
+    override val route = "pendapatan"
+}
+
+object DestinasiPengeluaran : AlamatNavigasi {
+    override val route = "pengeluaran"
+}
+
+object DestinasiInsert : AlamatNavigasi {
+    override val route = "insert"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val Kode = "Kode"
+    val routeWithArg = "$route/{$Kode}"
+}
+object DestinasiDetailPengeluaran : AlamatNavigasi {
+    override val route = "detailPengeluaran"
+    const val ID_ASET = "idAset"
+    val routeWithArg = "$route/{$ID_ASET}"
+
+    // Menambahkan titleRes
+    val titleRes = "Detail Pengeluaran"
+}
+object DestinasiDetailKategori : AlamatNavigasi {
+    override val route = "detailKategori"
+    const val idKategori = "idKategori"
+    val routeWithArg = "$route/{$idKategori}"
+}
+
+object DestinasiDetailAset : AlamatNavigasi {
+    override val route = "detailAset"
+    const val ID_ASET = "idAset"
+    val routeWithArg = "$route/{$ID_ASET}"
+
+    // Menambahkan titleRes
+    val titleRes = "Detail Aset"
 }
