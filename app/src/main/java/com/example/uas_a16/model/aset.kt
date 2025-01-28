ee8746bc4d3c83142064e708819a782822ccdbdf
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:37:13 +0700

    membuat table aset

--- app/src/main/java/com/example/uas_a16/model/aset.kt
+++ app/src/main/java/com/example/uas_a16/model/aset.kt
@@ -1,2 +1,12 @@
 package com.example.uas_a16.model
 
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "aset")
+data class Aset(
+    @PrimaryKey(autoGenerate = true) val idAset: Int = 0,
+    val namaAset: String,
+    val idKategori: Int
+)
