02ef8706505a548afb8f48ce4137a944ef6ad9db
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:53:25 +0700

    Finisingg updateViewKategori

--- app/src/main/java/com/example/uas_a16/ui/view/kategori/UpdateViewKategori.kt
+++ app/src/main/java/com/example/uas_a16/ui/view/kategori/UpdateViewKategori.kt
@@ -4,13 +4,106 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas_a16.model.Kategori
+import com.example.uas_a16.ui.ViewModel.aset.UpdateAsetViewModel
 import com.example.uas_a16.ui.ViewModel.aset.UpdateUiEvent
+import com.example.uas_a16.ui.ViewModel.aset.UpdateUiState
+import com.example.uas_a16.ui.ViewModel.kategori.UpdateKategoriViewModel
+import com.example.uas_a16.ui.view.Aset.CostumeTopAppBar
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateAsetScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate: () -> Unit,
+    viewModel: UpdateAsetViewModel = viewModel(),
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = "Update Aset", // Title for Update Aset
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateAsetBody(
+            updateUiState = viewModel.uiState,
+            onAsetValueChange = viewModel::updateAsetState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateAset(
+                        onSuccess = {
+                            onNavigate()
+                        },
+                        onError = { message ->
+                            // Handle error if needed
+                        }
+                    )
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateAsetBody(
+    updateUiState: UpdateUiState,
+    onAsetValueChange: (UpdateUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputAset(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onAsetValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
 
 @Composable
 fun FormInputAset(
