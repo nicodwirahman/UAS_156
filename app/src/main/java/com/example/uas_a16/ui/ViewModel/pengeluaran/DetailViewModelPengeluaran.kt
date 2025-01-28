9a4175dfcc0eee7b83b3d2935dad1c1ddc8309b3
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:09:06 +0700

    membuat data class pengeluaranUistate

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/pengeluaran/DetailViewModelPengeluaran.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/pengeluaran/DetailViewModelPengeluaran.kt
@@ -1,2 +1,22 @@
 package com.example.uas_a16.ui.ViewModel.pengeluaran
 
+import androidx.compose.runtime.getValue
+
+import com.example.uas_a16.model.Pengeluaran
+
+
+data class DetailPengeluaranUiState(
+    val detailUiEvent: InsertPengeluaranEvent = InsertPengeluaranEvent(),
+    val pengeluaranList: List<Pengeluaran> = emptyList(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+    val isSuccess: Boolean = false,
+    val successMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertPengeluaranEvent()
+
+    val isUiEventNotEmpty: Boolean // Tambahkan properti ini
+        get() = detailUiEvent != InsertPengeluaranEvent()
+}
