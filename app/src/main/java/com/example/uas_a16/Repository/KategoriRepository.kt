f4a7420f932235810ea54dacceed69d89f27a0af
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:17:54 +0700

    membuat KategoriRepository

--- app/src/main/java/com/example/uas_a16/Repository/KategoriRepository.kt
+++ app/src/main/java/com/example/uas_a16/Repository/KategoriRepository.kt
@@ -1,2 +1,30 @@
 package com.example.uas_a16.Repository
 
+import androidx.lifecycle.LiveData
+import com.example.uas_a16.DAO.KategoriDao
+import com.example.uas_a16.model.Kategori
+
+class KategoriRepository(private val kategoriDao: KategoriDao) {
+
+    // Mendapatkan seluruh data Kategori
+    val allKategori: LiveData<List<Kategori>> = kategoriDao.getAllKategori()
+
+    // Menambahkan Kategori baru
+    suspend fun insertKategori(kategori: Kategori) {
+        kategoriDao.insertKategori(kategori)
+    }
+
+    // Menghapus Kategori
+    suspend fun deleteKategori(kategori: Kategori) {
+        kategoriDao.deleteKategori(kategori)
+    }
+
+    // Memperbarui Kategori
+    suspend fun updateKategori(kategori: Kategori) {
+        kategoriDao.updateKategori(kategori)
+    }
+    // Mendapatkan Kategori berdasarkan ID
+    fun getKategoriById(idKategori: Int): LiveData<Kategori> {
+        return kategoriDao.getKategoriById(idKategori)
+    }
+}
