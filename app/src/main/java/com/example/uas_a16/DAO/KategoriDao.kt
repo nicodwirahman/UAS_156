bf65a2f7756993d30ec4084671e120a24bef9f79
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:10:55 +0700

    membuat dan amanmabah insert kategoridao

--- app/src/main/java/com/example/uas_a16/DAO/KategoriDao.kt
+++ app/src/main/java/com/example/uas_a16/DAO/KategoriDao.kt
@@ -1,4 +1,19 @@
 package com.example.uas_a16.DAO
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.uas_a16.model.Kategori
+import retrofit2.http.Headers
+
+@Dao
 interface KategoriDao {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertKategori(kategori: Kategori)
+
+
 }
