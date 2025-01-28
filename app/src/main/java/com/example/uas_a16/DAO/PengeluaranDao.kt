adcf58f6013c76899715d916f63045aca27c3990
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:16:31 +0700

    mengambil data pendapatan

--- app/src/main/java/com/example/uas_a16/DAO/PengeluaranDao.kt
+++ app/src/main/java/com/example/uas_a16/DAO/PengeluaranDao.kt
@@ -3,6 +3,7 @@ package com.example.uas_a16.DAO
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.example.uas_a16.model.Pendapatan
 import com.example.uas_a16.model.Pengeluaran
 import retrofit2.http.Headers
 
@@ -25,4 +26,21 @@ interface PengeluaranDao {
         // Mengupdate data pengeluaran
     @Update
     suspend fun updatePengeluaran(pengeluaran: Pengeluaran)
+
+
+    // Mengambil pendapatan berdasarkan ID Aset
+    @Query("SELECT * FROM pendapatan WHERE idAset = :idAset")
+    fun getPendapatanByAset(idAset: Int): LiveData<List<Pendapatan>>
+
+    // Mengambil pendapatan berdasarkan ID Kategori
+    @Query("SELECT * FROM pendapatan WHERE idKategori = :idKategori")
+    fun getPendapatanByKategori(idKategori: Int): LiveData<List<Pendapatan>>
+
+    // Mengambil seluruh data pendapatan
+    @Query("SELECT * FROM pendapatan")
+    fun getAllPendapatan(): LiveData<List<Pendapatan>>
+
+    // Menghitung total pendapatan
+    @Query("SELECT SUM(total) FROM pendapatan")
+    fun getTotalPendapatan(): LiveData<Double>
 }
