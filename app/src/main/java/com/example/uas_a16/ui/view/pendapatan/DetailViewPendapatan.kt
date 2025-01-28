18edf0b113df3c4d3da5ed90f0b3b001a9537542
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:47:16 +0700

    object DestinasiDetailPendapatan

--- app/src/main/java/com/example/uas_a16/ui/view/pendapatan/DetailViewPendapatan.kt
+++ app/src/main/java/com/example/uas_a16/ui/view/pendapatan/DetailViewPendapatan.kt
@@ -1,2 +1,15 @@
 package com.example.uas_a16.ui.view.pendapatan
 
+
+import com.example.uas_a16.ui.navigasi.AlamatNavigasi
+
+
+
+object DestinasiDetailPendapatan : AlamatNavigasi {
+    override val route = "detailPendapatan"
+    const val ID_ASET = "idAset"
+    val routeWithArg = "$route/{$ID_ASET}"
+
+    // Title untuk tampilan
+    val titleRes = "Detail Pendapatan"
+}
