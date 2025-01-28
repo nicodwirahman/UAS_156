00a3401c0bc04da87de1fff5b22c8c0a8955234d
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 16:17:35 +0700

    membuat asetRepository

--- app/src/main/java/com/example/uas_a16/Repository/AsetRepository.kt
+++ app/src/main/java/com/example/uas_a16/Repository/AsetRepository.kt
@@ -1,2 +1,31 @@
 package com.example.uas_a16.Repository
 
+import androidx.lifecycle.LiveData
+import com.example.uas_a16.DAO.AsetDao
+import com.example.uas_a16.model.Aset
+class AsetRepository(private val asetDao: AsetDao) {
+
+    // Mendapatkan seluruh data Aset
+    val allAset: LiveData<List<Aset>> = asetDao.getAllAset()
+
+    // Menambahkan Aset baru
+    suspend fun insertAset(aset: Aset) {
+        asetDao.insertAset(aset)
+    }
+
+    // Menghapus Aset
+    suspend fun deleteAset(aset: Aset) {
+        asetDao.deleteAset(aset)
+    }
+
+    // Mendapatkan Aset berdasarkan ID
+    fun getAsetById(idAset: Int): LiveData<Aset> {
+        return asetDao.getAsetById(idAset)
+    }
+
+
+    // Mengupdate Aset
+    suspend fun updateAset(aset: Aset) {
+        asetDao.updateAset(aset)
+    }
+}
