11c1fa12100a34868fd35f199a72cd258d64baa2
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:56:14 +0700

    PencatatanApp

--- app/src/main/java/com/example/uas_a16/PencatatanApp.kt
+++ app/src/main/java/com/example/uas_a16/PencatatanApp.kt
@@ -1,2 +1,33 @@
 package com.example.uas_a16
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.uas_a16.ui.navigasi.PengelolaHalaman
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PencatatanApp(
+    modifier: Modifier
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        //topBar = { TopAppBar(scrollBehavior = scrollBehavior) },
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalaman()
+        }
+    }
+}
