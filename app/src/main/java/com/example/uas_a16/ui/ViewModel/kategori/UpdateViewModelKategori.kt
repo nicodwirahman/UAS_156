a2c0d071836870b04d0ba96490d96a3a52392c53
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:13:08 +0700

    membuat sealed class

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/kategori/UpdateViewModelKategori.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/kategori/UpdateViewModelKategori.kt
@@ -1,2 +1,9 @@
 package com.example.uas_a16.ui.ViewModel.kategori
 
+
+
+sealed class UpdateKategoriUiState {
+    object Success : UpdateKategoriUiState()
+    object Error : UpdateKategoriUiState()
+    object Loading : UpdateKategoriUiState()
+}
