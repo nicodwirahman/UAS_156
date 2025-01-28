925bd1d38651165abc5f433adcd6ce80b7ac6b59
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:44:58 +0700

    EntryBody

--- app/src/main/java/com/example/uas_a16/ui/view/pendapatan/UpdateViewPendapatan.kt
+++ app/src/main/java/com/example/uas_a16/ui/view/pendapatan/UpdateViewPendapatan.kt
@@ -1,16 +1,47 @@
 package com.example.uas_a16.ui.view.pendapatan
 
+
+
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.unit.dp
+
 import com.example.uas_a16.ui.ViewModel.pendapatan.InsertPendapatanEvent
+import com.example.uas_a16.ui.ViewModel.pendapatan.InsertPendapatanUiState
 
 
 
+@Composable
+fun EntryBody(
+    insertUiState: InsertPendapatanUiState,
+    onPendapatanValueChange: (InsertPendapatanEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertPendapatanEvent,
+            onValueChange = onPendapatanValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
 
-
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
 
 @Composable
 fun FormInput(
