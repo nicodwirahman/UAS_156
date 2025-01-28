5c855b27d4431ab2c9247efdc43d351351568770
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:41:24 +0700

    ComponenDetailPengluaran

--- app/src/main/java/com/example/uas_a16/ui/view/pengeluaran/DetailViewPengeluaran.kt
+++ app/src/main/java/com/example/uas_a16/ui/view/pengeluaran/DetailViewPengeluaran.kt
@@ -44,3 +44,27 @@ object DestinasiDetailPengeluaran: AlamatNavigasi {
     // Menambahkan titleRes
     val titleRes = "Detail Pengeluaran"
 }
+
+@Composable
+fun ComponentDetailPengeluaran(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
