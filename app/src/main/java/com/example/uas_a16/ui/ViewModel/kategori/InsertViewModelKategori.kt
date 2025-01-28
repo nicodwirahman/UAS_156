d82555d16e091d07d59376091420f52f7ff46105
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:11:24 +0700

    Membuat Isaeled class

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/kategori/InsertViewModelKategori.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/kategori/InsertViewModelKategori.kt
@@ -1,2 +1,6 @@
 package com.example.uas_a16.ui.ViewModel.kategori
-
+sealed class InsertKategoriUiState {
+    object Success : InsertKategoriUiState()  // Ketika insert kategori berhasil
+    object Error : InsertKategoriUiState()   // Ketika terjadi error saat insert kategori
+    object Loading : InsertKategoriUiState() // Ketika proses insert kategori sedang berlangsung
+}
