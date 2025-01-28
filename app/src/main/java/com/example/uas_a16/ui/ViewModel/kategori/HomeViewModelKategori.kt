c4e9c9827c2d99bf58dd6842bbe89c51908d48d8
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 18:15:31 +0700

    membuat sealed class

--- app/src/main/java/com/example/uas_a16/ui/ViewModel/kategori/HomeViewModelKategori.kt
+++ app/src/main/java/com/example/uas_a16/ui/ViewModel/kategori/HomeViewModelKategori.kt
@@ -1,2 +1,12 @@
 package com.example.uas_a16.ui.ViewModel.kategori
 
+
+import com.example.uas_a16.model.Kategori
+
+
+sealed class HomeKategoriUiState {
+    data class Success(val kategori: List<Kategori>) : HomeKategoriUiState()
+    object Error : HomeKategoriUiState()
+    object Loading : HomeKategoriUiState()
+}
+
