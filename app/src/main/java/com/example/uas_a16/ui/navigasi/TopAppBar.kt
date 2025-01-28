b85e1a1329aa80c036065d20b5404cf1e7606197
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-28 19:54:54 +0700

    TopAppBar

--- app/src/main/java/com/example/uas_a16/ui/navigasi/TopAppBar.kt
+++ app/src/main/java/com/example/uas_a16/ui/navigasi/TopAppBar.kt
@@ -1,2 +1,48 @@
 package com.example.uas_a16.ui.navigasi
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = title) },
+        actions = {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "Refresh",
+                modifier = Modifier.clickable { onRefresh() }
+            )
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Navigate Back"
+                    )
+                }
+            }
+        }
+    )
+}
